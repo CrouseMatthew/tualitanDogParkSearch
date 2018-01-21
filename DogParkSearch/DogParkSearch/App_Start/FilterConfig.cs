07ca1cb901a592caefc7ef60f0ddcfa43b2a9c3a
CrouseMatthew <mscrouse@gmail.com>
2018-01-20 18:41:23 -0800

    Inital commit

---
+++ DogParkSearch/DogParkSearch/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DogParkSearch
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
